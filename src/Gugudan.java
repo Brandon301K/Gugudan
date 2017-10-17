be24efaa6e5c9d64ce3ec518ebd3b391b6d4c20e
Brandon Kim <Brandon Kim@DESKTOP-3JE396F>
2017-10-18 02:27:42 +0900

    구구단에 미리 for문 돌림 ㅋㅋㅋ

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
 	public static void main(String[] args) {
 		
 		// 2단
-		System.out.println(2 * 1);
+		/*System.out.println(2 * 1);
 		System.out.println(2 * 2);
 		System.out.println(2 * 3);
 		System.out.println(2 * 4);
@@ -13,9 +14,10 @@ public class Gugudan {
 		System.out.println(2 * 7);
 		System.out.println(2 * 8);
 		System.out.println(2 * 9);
+		System.out.println();*/
 		
 		// 3단
-		System.out.println(3 * 1);
+		/*System.out.println(3 * 1);
 		System.out.println(3 * 2);
 		System.out.println(3 * 3);
 		System.out.println(3 * 4);
@@ -24,6 +26,70 @@ public class Gugudan {
 		System.out.println(3 * 7);
 		System.out.println(3 * 8);
 		System.out.println(3 * 9);
+		System.out.println();*/
+		
+		/*// 4단
+		int number = 4;
+		int result = number * 1;
+		System.out.println(result);
+		result = number * 2;
+		System.out.println(result);
+		result = number * 3;
+		System.out.println(result);
+		result = number * 4;
+		System.out.println(result);
+		result = number * 5;
+		System.out.println(result);
+		result = number * 6;
+		System.out.println(result);
+		result = number * 7;
+		System.out.println(result);
+		result = number * 8;
+		System.out.println(result);
+		result = number * 9;
+		System.out.println(result);
+		System.out.println();*/
+		
+		/*// 5단
+		int number5 = 5;
+		int result5 = number5 * 1;
+		System.out.println(result5);
+		result5 = number5 * 2;
+		System.out.println(result5);
+		result5 = number5 * 3;
+		System.out.println(result5);
+		result5 = number5 * 4;
+		System.out.println(result5);
+		result5 = number5 * 5;
+		System.out.println(result5);
+		result5 = number5 * 6;
+		System.out.println(result5);
+		result5 = number5 * 7;
+		System.out.println(result5);
+		result5 = number5 * 8;
+		System.out.println(result5);
+		result5 = number5 * 9;
+		System.out.println(result5);
+		System.out.println();*/
+		
+		System.out.println("구구단중 출력할 단은?: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		int i;
+		for(i=1; i<10; i++) {
+			
+			System.out.println(number * i);
+		}
+		/*System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);*/
+		
 		
 		
 		
