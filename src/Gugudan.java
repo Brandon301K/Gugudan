68cd6c6d17817172dba160746a3ac74948cd5fbc
Brandon Kim <Brandon Kim@DESKTOP-3JE396F>
2017-10-23 13:47:08 +0900

    while문으로 오류시 계속 값을 요구하기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -105,22 +105,30 @@ public class Gugudan {
 			for(int i = 1; i < 10; i++) {
 				System.out.println(number * i);
 			}
+		}
+		
+		
+		boolean run = true;
+		while(run){
+			
+			System.out.println("구구단중 출력할 단은?: ");
+			Scanner scanner = new Scanner(System.in);
+			int number = scanner.nextInt();
+			System.out.println("사용자가 입력한 값: " + number);
+			
+			
+			if(number < 2 || number >10) {
+				System.out.println("구구단 범위가 아닙니다.");
+				
+			} else {
+				for(int i = 1; i < 10; i++) {
+					System.out.println(number * i);
+				}
+				break;
+			}
+			
 		}*/
 		
-		
-		System.out.println("구구단중 출력할 단은?: ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한 값: " + number);
-		
-		
-		if(number < 2 || number >10) {
-			System.out.println("구구단 범위가 아닙니다.");
-		} else {
-			for(int i = 1; i < 10; i++) {
-				System.out.println(number * i);
-			}
-		}
 		
 		
 		
